1244a8afcf9fd33282b95c614b91169feef6edcf
Pouul <paulohuang04@gmail.com>
2026-03-01 22:19:30 -0300

    nao_finalizado

---
+++ lab01/AFD.c
@@ -0,0 +1,260 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void PertenceAFD(char *palavras, char *alfabeto, char *estados, char *estadoInicial, char *estadosFinais, char **transicoes, int linhasTransicoes) {
+    int ilp = 0; //indice para linhda de palavras
+    int icp = 0; //indice para colunas de palavras
+    int ilt = 0; //indice para linha de transições
+    int ict = 0; //indice para coluna de transições
+
+    char estadoAtual = estadoInicial[0];
+    // loop para percorrer as palavras
+    while(palavras[ilp] != '\n'){
+        //loop para percorrer os caracteres da palavra
+        while (palavras[ilp][icp] != '\n'){
+            char simbolo = palavras[ilp][icp];
+            boolean transicaoEncontrada = false;
+
+            //loop para achar a transição
+            while (transicaoEncontrada == false && transicoes[ilt][0] != '\n'){
+                while (transicoes[ilt][ict] != '\n'){
+                    if (transicoes[ilt][0] == estadoAtual && transicoes[ilt][1] == simbolo){
+                        estadoAtual = transicoes[ilt][2];
+                        transicaoEncontrada = true; 
+                        break;
+                    }
+                    ict++;
+                }
+                if (transicaoEncontrada == false){
+                    ilt++;
+                    ict = 0;
+                } 
+            }//saiu do loop de transições
+            if (transicaoEncontrada == false){
+                printf("Palavra não pertence ao AFD\n");
+                 //se não encontrou a transição, a palavra não pertence ao AFD
+            }
+            int i = 0;//
+            while (estadosFinais[i] != '\n'){
+                if (estadoAtual == estadosFinais[i]){
+                    return true; //se o estado atual é um estado final, a palavra pertence ao AFD
+                }
+                i++;
+            }
+            return false; //se o estado atual não é um estado final, a palavra não pertence ao AFD
+        }
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Uso: %s <arquivo>\n", argv[0]);
+        return 1;
+    }
+    
+    char linha[50];
+
+    //Iniciando o alfabeto
+    int tamanhoAlfabeto = 1;
+    char *Alfabeto = malloc(tamanhoAlfabeto * sizeof(char));
+
+    //Iniciando os estados
+    int tamanhoEstados = 1;
+    char *Estados = malloc(tamanhoEstados * sizeof(char)); 
+
+    //Iniciando os estadosIniciais
+    int tamanhoEstadosInicias = 1;
+    char *estadosIniciais = malloc(tamanhoEstadosInicias * sizeof(char));
+
+    //Iniciando os estadosFinais
+    int tamanhoEstadosFinais = 1;
+    char *estadosFinais = malloc(tamanhoEstadosFinais * sizeof(char));
+
+    //Iniciando transições
+    int linhasTransicoes = 1;
+    int colunasTransicoes = 3;
+    char **transicoes = malloc(linhasTransicoes * sizeof(char*));
+    for (int i = 0; i < linhasTransicoes; i++) {
+        transicoes[i] = malloc(colunasTransicoes * sizeof(char));
+    }
+
+    //Iniciando palavras
+    int linhasPalavras = 0;
+    int colunasPalavras = 0;
+    char **palavras = NULL;
+
+    FILE *arquivo = fopen(argv[1], "r");
+
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        if (linha[0] == '#') {
+            printf("%s\n", linha);
+            continue;
+        }
+        // se alfabeto
+        if (linha[0] == 'A') {
+            int il = 1; //começa a ler após o "A"
+            while (linha[il] != '\n'){//vai até \n
+                if (linha[il] == ' '){// se for espaço pula
+                    il++;
+                    continue;
+                }
+                Alfabeto[tamanhoAlfabeto - 1] = linha[il];
+                il++;
+                tamanhoAlfabeto++;//aumenta o tamanho do vetor alfabeto
+                Alfabeto = realloc(Alfabeto, tamanhoAlfabeto * sizeof(char));//aloca mais memoria
+                
+            }
+            printf("A: ");
+            for (int iA = 0; iA < tamanhoAlfabeto - 1; iA++) {
+                printf("%c ", Alfabeto[iA]);
+            }
+            printf("\n");
+            continue;
+        }
+        
+        if (linha[0] == 'Q') {
+            int il = 1; //começa a ler após o "Q"
+            while (linha[il] != '\n'){//vai até \n
+                if (linha[il] == ' '){// se for espaço pula
+                    il++;
+                    continue;
+                }
+                Estados[tamanhoEstados - 1] = linha[il];
+                il++;
+                tamanhoEstados++;//aumenta o tamanho do vetor estados
+                Estados = realloc(Estados, tamanhoEstados * sizeof(char));//aloca mais memoria
+                
+            }
+            printf("Q: ");
+            for (int iE = 0; iE < tamanhoEstados - 1; iE++) {
+                printf("%c ", Estados[iE]);
+            }
+            printf("\n");
+            continue;
+        }
+
+        if (linha[0] == 'q') {
+            int il = 1; //começa a ler após o "q"
+            while (linha[il] != '\n'){//vai até \n
+                if (linha[il] == ' '){// se for espaço pula
+                    il++;
+                    continue;
+                }
+                if(tamanhoEstadosInicias > 1){
+                    printf("Erro: mais de um estado inicial encontrado.\n");
+                    return 1;
+                }
+                estadosIniciais[tamanhoEstadosInicias - 1] = linha[il];
+                il++;
+                tamanhoEstadosInicias++;//aumenta o tamanho do vetor estados iniciais
+                
+                
+            }
+            printf("q: ");
+            for (int iEI = 0; iEI < tamanhoEstadosInicias - 1; iEI++) {
+                printf("%c ", estadosIniciais[iEI]);
+            }
+            printf("\n");
+            continue;
+        }
+
+        if (linha[0] == 'F') {
+            int il = 1; //começa a ler após o "F"
+            while (linha[il] != '\n'){//vai até \n
+                if (linha[il] == ' '){// se for espaço pula
+                    il++;
+                    continue;
+                }
+                estadosFinais[tamanhoEstadosFinais - 1] = linha[il];
+                il++;
+                tamanhoEstadosFinais++;//aumenta o tamanho do vetor estados finais
+                estadosFinais = realloc(estadosFinais, tamanhoEstadosFinais * sizeof(char));//aloca mais memoria
+                
+            }
+            printf("F: ");
+            for (int iEF = 0; iEF < tamanhoEstadosFinais - 1; iEF++) {
+                printf("%c ", estadosFinais[iEF]);
+            }
+            printf("\n");
+            continue;
+        }
+
+        if (linha[0] == 'T') {
+            int il = 1; //começa a ler após o "T" iL - linha do arquivo
+            int ic = 0; //coluna
+            while (linha[il] != '\n'){//vai até \n
+                if (linha[il] == ' '){// se for espaço pula
+                    il++;
+                    continue;
+                }
+                if (ic >= colunasTransicoes) {
+                    printf("Erro: número de colunas excedido para transições.\n");
+                    return 1;
+                }
+                
+                transicoes[linhasTransicoes - 1][ic] = linha[il];
+                il++;
+                ic++;
+            }
+            printf("T: ");
+            for (int iT = 0; iT < ic; iT++) {
+                printf("%c ", transicoes[linhasTransicoes - 1][iT]);
+            }
+
+            linhasTransicoes++;//aumenta o número de linhas
+            transicoes = realloc(transicoes, linhasTransicoes * sizeof(char*));
+            transicoes[linhasTransicoes - 1] = malloc(colunasTransicoes * sizeof(char));
+
+            
+            printf("\n");
+            continue;
+        } 
+        
+        
+        if (linha[0] == 'P') {
+            int il = 1; //começa a ler após o "T" iL - linha do arquivo
+            //coluna declarada no início do código
+            if (colunasPalavras == 0){
+                    linhasPalavras ++;
+                    palavras = realloc(palavras, linhasPalavras * sizeof(char*));
+                    palavras[linhasPalavras - 1] = NULL;
+            }
+            
+            while (linha[il] != '\n'){//vai até \n
+                if (linha[il] == ' '){// se for espaço pula
+                    il++;
+                    continue;
+                }
+
+                palavras[linhasPalavras - 1] = realloc(palavras[linhasPalavras - 1], (colunasPalavras + 1) * sizeof(char));
+                palavras[linhasPalavras - 1][colunasPalavras] = linha[il];
+                il++;
+                colunasPalavras++;
+                
+            }
+            
+            printf("P: ");
+            for (int iP = 0; iP < colunasPalavras; iP++) {
+                printf("%c ", palavras[linhasPalavras - 1][iP]);
+            }
+            
+            colunasPalavras = 0;//reinicia a coluna para a próxima palavra
+        
+            printf("\n");
+            continue;
+        }       
+    
+    }
+
+
+
+    fclose(arquivo);
+
+    return 0;
+}
